a46105d850aeb2604e02d6fd24effa855e3d0e1d
RogelioSan09 <rogeliosnchz@gmail.com>
2023-06-14 18:12:15 -0700

    Established the questions to user and Pushed questions onto the questions array.

--- js/index.js
+++ js/index.js
@@ -1,55 +1,57 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
+// Created an array of questions for user input
 const questions = [];
-
-var prompts = new Rx.Queries();
-inquirer.prompt(prompts);
-
-prompts.next({
+questions.push({
     type: 'input',
     message: 'What is your project title?',
     name: 'title'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Description of your program?',
     name: 'description'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Installation instructions?',
     name: 'installation'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Usage information?',
     name: 'usageInfo'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Contribution Guidelines?',
     name: 'guidelines'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Test Instructions?',
     name: 'testInstructions'
 });
-prompts.next({
+questions.push({
     type: 'input',
     message: 'Choose a license:',
     name: 'license',
     choices: ['MIT']
 });
+questions.push({
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'github'
+});
+questions.push({
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+});
 
-prompts.complete();
-
-inquirer.prompt(prompts).ui.process.subscribe(onEachAnswer, onError, onComplete);
-
-var newFile = 'title'
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    
 }
 
 // TODO: Create a function to initialize app
