6976d4e4437c0740004dd54b2540872241c110f8
RogelioSan09 <rogeliosnchz@gmail.com>
2023-06-14 18:36:00 -0700

    Minor update to index.js file. Typos, comments and spacing.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+
 // Created an array of questions for user input
 const questions = [];
 questions.push({
@@ -79,15 +81,18 @@ inquirer.prompt(questions).then((response) => {
     `;
 });
 
-// TODO: Create a function to write README file
+// Created a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('The file has been created!')
+    );
 }
 
-// TODO: Create a function to initialize app
+// Created a function to initialize app
 function init() {
+    var fileName = 'README.md';
     writeToFile(fileName, data);
 }
 
-// Function call to initialize app
+// Function called to initialize app
 init();
